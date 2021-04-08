ff617a56b30dce23f505298d74ad76e5324f4e67
sukyeongh <hsk10271@gmail.com>
2021-04-08 22:40:05 +0900

    구현 : 우유축제

---
+++ 20210408/bj14720_hsk.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const stdin = (process.platform === 'linux'
+  ? fs.readFileSync('/dev/stdin').toString()
+  : `7
+0 1 2 0 1 2 0`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let store_num = input(); //가게의 개수
+let store_list = input().split(' '); //가게 리스트
+
+let drink_count = 0;
+let next_milk = 0;
+
+for (let i = 0; i < store_num; i++) {
+  if (parseInt(store_list[i]) === next_milk) {
+    next_milk = (next_milk + 1) % 3;
+    drink_count++;
+  }
+}
+
+console.log(drink_count);
