4f710556c2010f056b72d6b53a5dc08afefb1506
sukyeongh <hsk10271@gmail.com>
2021-04-09 08:51:20 +0900

    구현 : 팰린드롬인지 확인하기

---
+++ 20210409/bj10988_hsk.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const stdin = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString() : `backjon`).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let word = input();
+let is_palindrome = 0;
+
+for (let i = 0; i < word.length / 2; i++) {
+  if (word[i] !== word[word.length - 1 - i]) {
+    is_palindrome = 0;
+    break;
+  } else {
+    is_palindrome = 1;
+  }
+}
+
+console.log(is_palindrome);
