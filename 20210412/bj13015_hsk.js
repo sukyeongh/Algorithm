c040eb056b729257caa13ef0eb78a0add17c09be
Deepred <deepred.kim@gmail.com>
2021-04-12 16:21:26 +0900

    Update bj13015_hsk.js

--- 20210412/bj13015_hsk.js
+++ 20210412/bj13015_hsk.js
@@ -3,6 +3,13 @@ const stdin = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toSt
 
 const N = parseInt(stdin[0]);
 
+//// 전체적으로 가독성이 많이 떨어집니다 ㅠㅠ
+//// 코드 작성 의도를 설명하는 수단으로 '변수 이름'과 '함수 이름'이 제일 중요한 역할을 한다고 생각하는데
+//// 변수 이름은 아무것도 설명하지 않고, 함수는 아예 만들어지지도 않아서 어떤 의도로 작성했는지를 알기 어려워요
+//// 혹 개선 방향을 잡기 힘들다면, 제 코드나 승빈이 코드가 참고가 되었으면 좋겠습니다
+  //// 진홍: https://www.acmicpc.net/source/24870384
+  //// 승빈: https://www.acmicpc.net/source/24882658
+
 //a,b,c,d를 맨 위 꼭짓점으로 설정하였음
 //a,b는 n번째 줄까지 증가했다가 n번째 이후 감소함
 //c,d는 n번쨰 줄까지 감소했다가 n번째 이후 증가함
