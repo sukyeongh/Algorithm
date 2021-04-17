34272be901e09d98ad284a55bb6d3b3da83d9a5b
sukyeongh <hsk10271@gmail.com>
2021-04-18 00:27:41 +0900

    스텍/큐/덱을 이용한 문제 : AC

---
+++ 20210418/bj5430_hsk.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const stdin = (process.platform === 'linux'
+  ? fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+  : `4
+RDD
+4
+[1,2,3,4]
+DD
+1
+[42]
+RRD
+6
+[1,1,2,3,5,8]
+D
+0
+[]`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const N = parseInt(input());
+const output = [];
+
+const ac = (list, commands) => {
+  let is_reverse = false;
+  for (const command of commands) {
+    if (command === 'R') is_reverse = !is_reverse;
+    else {
+      if (list.length == 0) return 'error';
+      if (is_reverse) list.pop();
+      else list.shift();
+    }
+  }
+  if (is_reverse) return '[' + list.reverse().join(',') + ']';
+  else return '[' + list.join(',') + ']';
+};
+
+for (let i = 0; i < N; i++) {
+  const commands = input().replace('RR', '').split('');
+  const list_size = input();
+  const list_element = input().replace('[', '').replace(']', '').split(',');
+
+  if (list_size == 0) list_element.pop();
+  output.push(ac(list_element, commands));
+}
+console.log(output.join('\n'));
