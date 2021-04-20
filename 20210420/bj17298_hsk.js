18811ea021a45845a1b21d35de2b31a076edaab7
Deepred <deepred.kim@gmail.com>
2021-04-20 15:46:00 +0900

    Update bj17298_hsk.js

--- 20210420/bj17298_hsk.js
+++ 20210420/bj17298_hsk.js
@@ -10,6 +10,7 @@ let list = stdin[1].split(' ').map((v) => parseInt(v));
 let stack = [];
 let result = new Array(N).fill(-1);
 
+//// 와 저 이거 당시에 포기했던 문제였는데 ㅠㅠㅋㅋ 깔끔하게 잘 풀어내셨네요
 for (let i = 0; i < N; i++) {
   while (stack.length !== 0 && list[stack[stack.length - 1]] < list[i]) {
     result[stack.pop()] = list[i];
