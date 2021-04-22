7456ae62580bf20d53df3df75d70d8547cc59963
Deepred <deepred.kim@gmail.com>
2021-04-22 10:14:36 +0900

    Update bj10829_hsk.js

--- 20210422/bj10829_hsk.js
+++ 20210422/bj10829_hsk.js
@@ -7,7 +7,7 @@ const changeToBinary = (N) => {
   let result = '';
 
   if (N <= 1) return result.concat(N);
-
+  //// 낯선 개념인 머리재귀를 바로 이해하고 쓰시네요. 앞으로의 재귀풀이가 기대됩니다 ㅎㅋㅋ
   return result.concat(changeToBinary(parseInt(N / 2)), N % 2);
 };
 
