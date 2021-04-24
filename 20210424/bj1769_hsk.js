6a78fc3d8fe03f8718865d997a167e84a50a51e4
Deepred <deepred.kim@gmail.com>
2021-04-24 11:05:51 +0900

    Update bj1769_hsk.js

--- 20210424/bj1769_hsk.js
+++ 20210424/bj1769_hsk.js
@@ -4,9 +4,12 @@ const stdin = (process.platform === 'linux'
   : `1234567`
 ).split('\n');
 
+//// parseInt했다가 도로 toString할거라면 parseInt를 하지 않는 쪽이 깔끔할 것 같아요!
 let N = parseInt(stdin);
 let converting_count = 0;
 
+//// 함수내에서 계산하고 출력하고 다 하는게 음.. 깔끔하지 않은 것 같습니다.
+//// 함수에서는 값을 계산하여 반환만 하고 출력은 별개로 하는건 어떨까요?
 const convertQuestion = (num) => {
   let num_list = num.toString().split('');
   if (num_list.length <= 1) {
