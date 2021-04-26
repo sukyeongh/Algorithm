ffbf9262c68c3419ee1fda6d70cf3fa61c6b2f04
sukyeongh <hsk10271@gmail.com>
2021-04-26 12:09:20 +0900

    재귀함수 문제 : 별 찍기 -11

---
+++ 20210426/bj2448_hsk.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const stdin = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `24`).split('\n');
+
+const N = parseInt(stdin);
+const starArr = Array.from(Array(N), () => Array(2 * N - 1).fill(''));
+
+const drawStar = (x, y, len) => {
+  if (len === 3) {
+    starArr[x][y] = '*';
+    starArr[x + 1][y - 1] = '*';
+    starArr[x + 1][y + 1] = '*';
+    for (let i = -2; i < 3; i++) {
+      starArr[x + 2][y + i] = '*';
+    }
+    return;
+  }
+
+  let currLen = parseInt(len / 2);
+  drawStar(x, y, currLen);
+  drawStar(x + currLen, y - currLen, currLen);
+  drawStar(x + currLen, y + currLen, currLen);
+};
+
+drawStar(0, N - 1, N);
+//가장 큰 삼각형부터
+
+for (var i = 0; i < N; i++) {
+  let result = '';
+  for (var j = 0; j < 2 * N - 1; j++) {
+    if (starArr[i][j] == '*') result += '*';
+    else result += ' ';
+  }
+  console.log(result);
+}
