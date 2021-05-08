b7f7b94af0f53c3351be4de7b4b0c717d0cee5d9
sukyeongh <hsk10271@gmail.com>
2021-05-08 17:48:31 +0900

    재귀함수 문제 : Moo 게임 (return 수정)

--- 20210507/bj5904_hsk.js
+++ 20210507/bj5904_hsk.js
@@ -1,11 +1,10 @@
 const fs = require('fs');
-const stdin = process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `11`;
+const stdin = process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `16`;
 
 const N = parseInt(stdin);
 
 let len = 3;
 let index = 0;
-let result = '';
 
 if (len >= 3) {
   while (len * 2 + 3 + index < N) {
@@ -18,23 +17,16 @@ if (len >= 3) {
 
 const Moo = (curr_num, index, len) => {
   if (index === 0) {
-    result = curr_num - 1 === 0 ? 'm' : 'o';
-    return;
+    return curr_num - 1 === 0 ? 'm' : 'o';
   }
 
   let before = parseInt((len - (3 + index)) / 2);
 
-  if (curr_num <= before) Moo(curr_num, index - 1, before);
+  if (curr_num <= before) return Moo(curr_num, index - 1, before);
   else if (before < curr_num && curr_num <= before + (3 + index)) {
-    if (curr_num === before + 1) {
-      result = 'm';
-      return;
-    } else {
-      result = 'o';
-      return;
-    }
-  } else Moo(curr_num - (before + (3 + index)), index - 1, before);
+    if (curr_num === before + 1) return 'm';
+    else return 'o';
+  } else return Moo(curr_num - (before + (3 + index)), index - 1, before);
 };
 
-Moo(N, index, len);
-console.log(result);
+console.log(Moo(N, index, len));
