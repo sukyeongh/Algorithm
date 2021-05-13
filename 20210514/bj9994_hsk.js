cf19d9ee79e1b0d70f9d2a24046f9e41f15ced24
sukyeongh <hsk10271@gmail.com>
2021-05-14 00:08:43 +0900

    재귀함수 활용 문제 : Secret Code (Upgrade)

--- 20210514/bj9994_hsk.js
+++ 20210514/bj9994_hsk.js
@@ -4,6 +4,13 @@ const stdin = process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toStr
 const S = stdin;
 let cnt_encryption = 0;
 
+const equalString = (str_a, str_b, encryption_str) => {
+  if (str_a === str_b) {
+    cnt_encryption++;
+    cntEncryption(encryption_str);
+  }
+};
+
 const cntEncryption = (encryption_str) => {
   let e_len = encryption_str.length;
   if (e_len % 2 === 0 || e_len === 1) return;
@@ -11,26 +18,14 @@ const cntEncryption = (encryption_str) => {
   let first_half = encryption_str.substr(0, e_len / 2);
   let last_half = encryption_str.substr(e_len / 2, e_len / 2 + 1);
 
-  if (first_half === last_half.substr(0, first_half.length)) {
-    cnt_encryption++;
-    cntEncryption(last_half);
-  }
-  if (first_half === last_half.substr(1, first_half.length)) {
-    cnt_encryption++;
-    cntEncryption(last_half);
-  }
+  equalString(first_half, last_half.substr(0, first_half.length), last_half);
+  equalString(first_half, last_half.substr(1, first_half.length), last_half);
 
   first_half = encryption_str.substr(0, e_len / 2 + 1);
   last_half = encryption_str.substr(e_len / 2 + 1, e_len / 2);
 
-  if (last_half === first_half.substr(0, last_half.length)) {
-    cnt_encryption++;
-    cntEncryption(first_half);
-  }
-  if (last_half === first_half.substr(1, last_half.length)) {
-    cnt_encryption++;
-    cntEncryption(first_half);
-  }
+  equalString(first_half.substr(0, last_half.length), last_half, first_half);
+  equalString(first_half.substr(1, last_half.length), last_half, first_half);
 };
 
 cntEncryption(S);
