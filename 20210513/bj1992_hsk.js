537b634dedd989c9fc728664703dcb798d1c83b9
Deepred <deepred.kim@gmail.com>
2021-05-13 20:56:29 +0900

    Update bj1992_hsk.js

--- 20210513/bj1992_hsk.js
+++ 20210513/bj1992_hsk.js
@@ -27,12 +27,14 @@ for (let i = 0; i < N; i++) {
 }
 
 const quardTree = (row, col, N) => {
+  //// is_visited라는 이름이 나오게 된 경위를 잘 모르겠습니다. 하나라도 같지 않다=방문한 것?
   let is_visited = false;
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
       if (pixels[row + i][col + j] !== pixels[row][col]) {
         is_visited = true;
+        //// 함수로 구현하는 편이 더 깔끔하지 않을까요? 플래그 변수를 쓰지 않아도 되는데다 이중 for문을 38~39줄 없이 return으로 쉽게 빠져나올 수 있으니까요!
         i = N;
         j = N;
       }
