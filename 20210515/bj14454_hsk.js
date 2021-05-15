ea52367479a21d390fa9c2c9083bcced0d16e864
sukyeongh <hsk10271@gmail.com>
2021-05-15 21:43:41 +0900

    재귀함수 활용 문제 : Secret Cow Code (실패)

--- 20210515/bj14454_hsk.js
+++ 20210515/bj14454_hsk.js
@@ -4,18 +4,18 @@ const stdin = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toSt
 let word = stdin[0];
 let N_idx = Number(stdin[1] - 1);
 
-let word_len = Number(word.length);
-let change_word_len = Number(word.length);
+let word_len = word.length;
+let change_word_len = word.length;
 
-const orderWord = (len, nth_word) => {
-  if (len === word_len) return nth_word;
+const orderWord = (len, nth_word_idx) => {
+  if (len == word_len) return nth_word_idx;
 
   let before_len = len / 2;
 
-  if (nth_word < before_len) return orderWord(before_len, nth_word);
+  if (before_len > nth_word_idx) return orderWord(before_len, nth_word_idx);
 
-  let is_before = nth_word - before_len - 1 < 0;
-  return orderWord(before_len, is_before ? nth_word - 1 : nth_word - before_len - 1);
+  let is_before_idx = nth_word_idx - before_len - 1 < 0;
+  return orderWord(before_len, is_before_idx ? nth_word_idx - 1 : nth_word_idx - before_len - 1);
 };
 
 while (change_word_len <= N_idx) {
