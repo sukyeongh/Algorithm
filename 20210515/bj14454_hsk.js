022d19153dfbc7b36477af26fcd01cc4264e1443
sukyeongh <hsk10271@gmail.com>
2021-05-15 22:15:36 +0900

    재귀함수 활용 문제 : Secret Cow Code (node.js 해결)

--- 20210515/bj14454_hsk.js
+++ 20210515/bj14454_hsk.js
@@ -1,25 +1,26 @@
+///모든 숫자 변수를 BitInt로 변경하면 가능함.
 const fs = require('fs');
 const stdin = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `COW 8`).split(' ');
 
 let word = stdin[0];
-let N_idx = Number(stdin[1] - 1);
+let N_idx = BigInt(stdin[1]) - BigInt(1);
 
-let word_len = word.length;
-let change_word_len = word.length;
+let word_len = BigInt(word.length);
+let change_word_len = BigInt(word.length);
 
 const orderWord = (len, nth_word_idx) => {
   if (len == word_len) return nth_word_idx;
 
-  let before_len = len / 2;
+  let before_len = BigInt(len) / BigInt(2);
 
   if (before_len > nth_word_idx) return orderWord(before_len, nth_word_idx);
 
-  let is_before_idx = nth_word_idx - before_len - 1 < 0;
-  return orderWord(before_len, is_before_idx ? nth_word_idx - 1 : nth_word_idx - before_len - 1);
+  let is_before_idx = nth_word_idx - before_len - BigInt(1) < 0;
+  return orderWord(before_len, is_before_idx ? nth_word_idx - BigInt(1) : nth_word_idx - before_len - BigInt(1));
 };
 
 while (change_word_len <= N_idx) {
-  change_word_len *= 2;
+  change_word_len *= 2n;
 }
 
 console.log(word[orderWord(change_word_len, N_idx)]);
