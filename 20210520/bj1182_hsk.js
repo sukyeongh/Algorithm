a764c93ffd31a36cd2b02154d71010d6079be795
sukyeongh <hsk10271@gmail.com>
2021-05-20 19:50:01 +0900

    비트마스킹 익히기 : 부분수열의 합
    - boj 1182
    - 부분집합은 비트 자리수로 계산하면 편함

---
+++ 20210520/bj1182_hsk.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `5 0
+-7 -3 -2 5 8`
+).split('\n');
+
+const input = stdin[0].split(' ');
+const arr = stdin[1].split(' ').map((value) => parseInt(value));
+
+const N = parseInt(input[0]);
+const S = parseInt(input[1]);
+let result = 0;
+
+let subset_num = 1 << N;
+for (let i = 1; i < subset_num; i++) {
+  let sum = 0;
+  for (let j = 0; j < N; j++) {
+    if ((i & (1 << j)) !== 0) sum += arr[j];
+  }
+  if (sum === S) result++;
+}
+
+console.log(result);
