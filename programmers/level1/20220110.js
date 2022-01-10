0586c63b5af699782543a71b39034e8633a6c5a9
sukyeongh <hsk10271@gmail.com>
2022-01-10 20:31:09 +0900

    Update 20220110.js

--- programmers/level1/20220110.js
+++ programmers/level1/20220110.js
@@ -1,11 +1,11 @@
 function solution(x) {
-  let dividedNum = x
+  let remainder = x
     .toString()
     .split('')
     .map(Number)
     .reduce((prev, cur) => prev + cur);
 
-  return x % dividedNum === 0 ? true : false;
+  return x % remainder === 0 ? true : false;
 }
 
 console.log(solution(13));
