c15d9151a78bcb1611fd988b654f8258a3c79ec9
sukyeongh <hsk10271@gmail.com>
2022-02-09 18:22:25 +0900

    Level1 : 2016년

---
+++ programmers/level1/20220209.js
@@ -0,0 +1,5 @@
+function solution(a, b) {
+  const day = { 0: 'SUN', 1: 'MON', 2: 'TUE', 3: 'WED', 4: 'THU', 5: 'FRI', 6: 'SAT' };
+  const date = new Date(`2016-${a}-${b}`);
+  return day[date.getDay()];
+}
