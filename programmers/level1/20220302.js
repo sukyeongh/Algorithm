230465a342a05bcd63e9761b9af92a7d8b1b8d29
sukyeongh <hsk10271@gmail.com>
2022-03-02 18:20:14 +0900

    Level1 : 소수 만들기

---
+++ programmers/level1/20220302.js
@@ -0,0 +1,24 @@
+const isPrime = (num) => {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};
+
+function solution(nums) {
+  let answer = 0;
+  const len = nums.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      for (let k = j + 1; k < len; k++) {
+        const sum = nums[i] + nums[j] + nums[k];
+        if (isPrime(sum)) {
+          answer++;
+        }
+      }
+    }
+  }
+  return answer;
+}
+
+console.log(solution([1, 2, 7, 6, 4]));
